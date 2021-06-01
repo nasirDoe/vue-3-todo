a7ac34c9ceec96b2cc3aefa46dc8ed6b0fa3df8d
Muhamad Nasir <bhio231nasir@gmail.com>
2021-06-01 09:11:29 +0700

    initial commit

---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="visible" class="modal__overlay">
+    <div class="modal">
+      <div class="modal__header"></div>
+      <div class="modal__body">
+        <slot name="body"></slot>
+      </div>
+      <div class="modal__footer"></div>
+    </div>
+  </div>
+</template>
+<script>
+import { defineComponent, watch } from 'vue'
+export default defineComponent ({
+  props: {
+    visible: {
+      default: false,
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup: (props, { emit }) => {
+    watch(
+      () => props.visible,
+      (visible) => {
+        if (visible === true) {
+          emit('on-show')
+        } else {
+          emit('on-close')
+        }
+      }
+    )
+  },
+})
+</script>
